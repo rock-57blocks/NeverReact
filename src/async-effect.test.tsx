e27b97f5e01c271b5862092f5f509f6b02036831
rock-57blocks <rchen@ext.liftoff.io>
2025-01-03 10:35:43 +0800

    tt

--- src/async-effect.test.tsx
+++ src/async-effect.test.tsx
@@ -1,10 +1,8 @@
 
 import { describe, expect, it } from '@jest/globals';
-import { fireEvent, render } from '@testing-library/preact';
-import TestPlayer, { muteVideo } from './test-player';
+import { render, waitFor } from '@testing-library/preact';
 import { act } from 'preact/test-utils';
-import { useSignal, useSignalEffect } from '@preact/signals';
-import { useRef, useEffect } from 'preact/hooks';
+import { useRef, useEffect, useState } from 'preact/hooks';
 
 describe('async', () => {
   function setupScratch(id?: string) {
@@ -18,33 +16,37 @@ describe('async', () => {
     let scratch = setupScratch();
     function App() {
       const videoRef = useRef<HTMLVideoElement>(null);
-      const hlsUrl = useSignal("");
+      const [hlsUrl, setHlsUrl] = useState("");
+
+
+      const getHlsUrl = async () => {
+        // this is the key issue here, if there is a promise in the useEffect, the test will fail
+        const url = await new Promise<string>(resolve => {
+          resolve("https://hlsurl.com/");
+        });
+        console.log("url changed", url);
+        setHlsUrl(url);
+      };
 
       useEffect(() => {
-        const getHlsUrl = async () => {
-          // this is the key issue here, if there is a promise in the useEffect, the test will fail
-          const url = await new Promise<string>(resolve => {
-            resolve("https://hlsurl.com/");
-          });
-          console.log("url changed", url);
-          hlsUrl.value = url;
-        };
 
         getHlsUrl();
       }, []);
 
-      useSignalEffect(() => {
-        console.log("call useSignalEffect with hlsUrl.value", hlsUrl.value);
-        videoRef.current!.src = hlsUrl.value;
-      });
+      useEffect(() => {
+        console.log("call useSignalEffect with hlsUrl", hlsUrl);
+        videoRef.current!.src = hlsUrl;
+      }, [hlsUrl]);
 
       return <video data-testid="video-player" ref={videoRef}></video>;
     }
 
-    await act(() => {
+    await act(async () => {
       render(<App />, { container: scratch });
     });
     const video = scratch.firstChild as HTMLVideoElement;
-    expect(video.src).toBe("https://hlsurl.com/");
+    await waitFor(() => {
+      expect(video.src).toBe("https://hlsurl.com/");
+    });
   });
 });
